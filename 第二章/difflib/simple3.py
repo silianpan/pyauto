9518691d5049d8dee25cfc5ff1c899d187673d92
liupan <1206284818@qq.com>
2020-01-30 17:13:01 +0800

    文件目录对比

--- 第二章/difflib/simple3.py
+++ 第二章/difflib/simple3.py
@@ -3,30 +3,32 @@ import difflib
 import sys
 
 try:
-    textfile1=sys.argv[1]
-    textfile2=sys.argv[2]
-except Exception,e:
-    print "Error:"+str(e)
-    print "Usage: simple3.py filename1 filename2"
+    textfile1 = sys.argv[1]
+    textfile2 = sys.argv[2]
+except Exception as e:
+    print("Error:"+str(e))
+    print("Usage: simple3.py filename1 filename2")
     sys.exit()
+
 
 def readfile(filename):
     try:
-        fileHandle = open (filename, 'rb' ) 
-        text=fileHandle.read().splitlines()
+        fileHandle = open(filename, 'rb')
+        text = fileHandle.read().splitlines()
         fileHandle.close()
         return text
     except IOError as error:
-       print('Read file Error:'+str(error))
-       sys.exit()
+        print('Read file Error:'+str(error))
+        sys.exit()
 
-if textfile1=="" or textfile2=="":
-    print "Usage: simple3.py filename1 filename2"
+
+if textfile1 == "" or textfile2 == "":
+    print("Usage: simple3.py filename1 filename2")
     sys.exit()
 
 
-text1_lines = readfile(textfile1) 
-text2_lines = readfile(textfile2) 
+text1_lines = readfile(textfile1)
+text2_lines = readfile(textfile2)
 
 d = difflib.HtmlDiff()
-print d.make_file(text1_lines, text2_lines)
+print(d.make_file(text1_lines, text2_lines))
