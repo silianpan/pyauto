cdc239c3c171cff64387151fb7ec206e547464e7
liupan <1206284818@qq.com>
2020-01-31 12:06:37 +0800

    scapy

--- 第三章/rrdtool/update.py
+++ 第三章/rrdtool/update.py
@@ -1,11 +1,17 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/python
-import rrdtool
-import time,psutil
- 
-total_input_traffic = psutil.net_io_counters()[1]
-total_output_traffic = psutil.net_io_counters()[0]
-starttime=int(time.time())
+# -*- coding: utf-8 -*-
 
-update=rrdtool.updatev('/home/test/rrdtool/Flow.rrd','%s:%s:%s' % (str(starttime),str(total_input_traffic),str(total_output_traffic)))
-print update 
+import rrdtool
+import time
+import psutil
+
+# 获取网卡入流量
+total_input_traffic = psutil.net_io_counters()[1]
+# 获取网卡出流量
+total_output_traffic = psutil.net_io_counters()[0]
+starttime = int(time.time())
+
+# 将三个参数作为updatev参数，返回{'return_value':0L}则更新成功，反之失败
+update = rrdtool.updatev('/home/test/rrdtool/Flow.rrd', '%s:%s:%s' %
+                         (str(starttime), str(total_input_traffic), str(total_output_traffic)))
+print(update)
--- 第三章/scapy/simple1.py
+++ 第三章/scapy/simple1.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
-import os,sys,time,subprocess
-import warnings,logging
+from scapy.all import traceroute
+import time
+import subprocess
+import warnings
+import logging
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import traceroute
 domains = input('Please input one or more IP/domain: ')
-target =  domains.split(' ')
+target = domains.split(' ')
 dport = [80]
-if len(target) >= 1 and target[0]!='':
-    res,unans = traceroute(target,dport=dport,retry=-2)
+if len(target) >= 1 and target[0] != '':
+    res, unans = traceroute(target, dport=dport, retry=-2)
     res.graph(target="> test.svg")
     time.sleep(1)
     subprocess.Popen("/usr/bin/convert test.svg test.png", shell=True)
 else:
-    print "IP/domain number of errors,exit"
+    print("IP/domain number of errors,exit")
