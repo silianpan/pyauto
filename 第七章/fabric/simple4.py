ada2614b266221c77c2b52953126c9360b04682e
liupan <1206284818@qq.com>
2020-02-10 23:36:25 +0800

    打包传递校验文件

--- 第七章/fabric/simple4.py
+++ 第七章/fabric/simple4.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python
-from fabric.api import *
-from fabric.context_managers import *
-from fabric.contrib.console import confirm
+from fabric import task, Connection
 
-env.user = 'root'
-env.hosts = ['192.168.1.21', '192.168.1.22', '192.168.1.23']
-env.password = 'SKJh935yft#'
+# env.user = 'root'
+# env.hosts = ['192.168.1.21', '192.168.1.22', '192.168.1.23']
+# env.password = 'SKJh935yft#'
+from fabric.transfer import Transfer
+from invoke import Exit
 
-
-@task
-@runs_once
-def tar_task():
-    with lcd("/data/logs"):
-        local("tar -czf access.tar.gz access.log")
+connection = Connection(host='192.168.1.2', user='root',
+                        connect_kwargs={'password': 'password'})
 
 
 @task
-def put_task():
-    run("mkdir -p /data/logs")
-    with cd("/data/logs"):
-        with settings(warn_only=True):
-            result = put("/data/logs/access.tar.gz",
-                         "/data/logs/access.tar.gz")
-        if result.failed and not confirm("put file failed, Continue[Y/N]?"):
-            abort("Aborting file put task!")
+def tar_task(c):
+    c.run('cd /data/logs && tar -czf access.tar.gz access.log')
 
 
 @task
-def check_task():
-    with settings(warn_only=True):
-        lmd5 = local("md5sum /data/logs/access.tar.gz",
-                     capture=True).split(' ')[0]
-        rmd5 = run("md5sum /data/logs/access.tar.gz").split(' ')[0]
+def put_task(c):
+    connection.run("mkdir -p /data/logs")
+    try:
+        c.run('/data/logs')
+        Transfer(connection).put('/data/logs/access.tar.gz', '/data/logs/access.tar.gz')
+    except OSError as e:
+        raise Exit("Aborting file put task!" + str(e))
+
+
+@task
+def check_task(c):
+    lmd5 = c.run('md5sum /data/logs/access.tar.gz', capture=True).split(' ')[0]
+    rmd5 = connection.run("md5sum /data/logs/access.tar.gz").split(' ')[0]
     if lmd5 == rmd5:
-        print "OK"
+        print("OK")
     else:
-        print "ERROR"
+        print("ERROR")
 
 
-@task
+# @task
 def go():
     tar_task()
     put_task()
     check_task()
+
+
+if __name__ == '__main__':
+    go()
