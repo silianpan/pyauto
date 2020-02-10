d4d709bcefe7971c8785d2b7973a3bf9d2304a79
liupan <1206284818@qq.com>
2020-02-10 23:15:18 +0800

    通过堡垒机传递文件

--- 第七章/fabric/simple3.py
+++ 第七章/fabric/simple3.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python
-from fabric.api import *
-from fabric.context_managers import *
-from fabric.contrib.console import confirm
+from fabric import ThreadingGroup, Connection
 
-env.user = 'root'
-env.gateway = '192.168.1.23'
-env.hosts = ['192.168.1.21', '192.168.1.22']
-env.passwords = {
-    'root@192.168.1.21:22': 'SKJh935yft#',
-    'root@192.168.1.22:22': 'SKJh935yft#',
-    'root@192.168.1.23:22': 'KJSD9325hgs'
-}
+# env.user = 'root'
+# env.gateway = '192.168.1.23'
+# env.hosts = ['192.168.1.21', '192.168.1.22']
+# env.passwords = {
+#     'root@192.168.1.21:22': 'SKJh935yft#',
+#     'root@192.168.1.22:22': 'SKJh935yft#',
+#     'root@192.168.1.23:22': 'KJSD9325hgs'
+# }
+from fabric.transfer import Transfer
+from invoke import Exit
 
-lpackpath = "/home/install/lnmp0.9.tar.gz"
+group = ThreadingGroup(
+    "172.16.95.1", user="root",
+    gateway=Connection(host='113.62.127.199', user='root',
+                       connect_kwargs={'password': 'Leiwuqi_891'}),
+    connect_kwargs={'password': 'Leiwuqi_891'},
+)
+
+lpackpath = "./fileTestTranster.txt"
 rpackpath = "/tmp/install"
 
 
-@task
+# @task
 def put_task():
-    run("mkdir -p /tmp/install")
-    with settings(warn_only=True):
-        result = put(lpackpath, rpackpath)
-    if result.failed and not confirm("put file failed, Continue[Y/N]?"):
-        abort("Aborting file put task!")
+    group.run("mkdir -p /tmp/install")
+    for conn in group:
+        try:
+            Transfer(conn).put(lpackpath, rpackpath)
+        except OSError as e:
+            raise Exit("Aborting file put task!" + str(e))
 
 
-@task
+# @task
 def run_task():
-    with cd("/tmp/install"):
-        run("tar -zxvf lnmp0.9.tar.gz")
-        with cd("lnmp0.9/"):
-            run("./centos.sh")
+    group.run("cd /tmp/install && cat fileTestTranster.txt")
 
 
-@task
+# @task
 def go():
     put_task()
     run_task()
+
+
+if __name__ == '__main__':
+    go()
