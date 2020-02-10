ec50333c8fcb2205944948e7077ac37ef7bedbb7
liupan <1206284818@qq.com>
2020-02-10 17:30:18 +0800

    远程run命令

--- 第七章/fabric/fabfile.py
+++ 第七章/fabric/fabfile.py
@@ -3,11 +3,11 @@ from fabric import task
 
 '''
 fab命令引用默认文件名为fabfile.py，如果使用非默认文件名称，则需通过’-f‘来指定
-如：fab -H SN2013 -f host_type.py host_type。
+如：fab -H hostname/ip -f host_type.py host_type。
 如果管理机和目标主机未配置密钥认证信任，将会提示输入目标主机对应账号登陆密码
 
 有时不需要写一行Python代码也可以完成远程操作，直接使用命令行的形式，如：
-fab -p xxx -H 192.168.1.2,192.168.1.3 -- 'uname -s'
+fab -p password -H 192.168.1.2,192.168.1.3 -- 'uname -s'
 '''
 
 
@@ -16,15 +16,3 @@ def host_type(c):
     # c = Connection(host='192.168.1.2', user='root',
     #                connect_kwargs={'password': 'password'})
     c.run('uname -s')
-
-
-@task
-def local_task(c):
-    c.run("uname -a")
-
-
-@task
-def remote_task(c):
-    # 两个命令
-    with c.run("/data/logs"):
-        c.run("ls -l")
--- 第七章/fabric/simple1.py
+++ 第七章/fabric/simple1.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-from fabric import task
 
 # env.user = 'root'
 # env.hosts = ['192.168.1.21', '192.168.1.22']
 # env.password = 'SKJh935yft#'
+from fabric import ThreadingGroup
+
+group = ThreadingGroup(
+    "192.168.1.1", "192.168.1.2", user="root", connect_kwargs={'password': 'password'},
+)
 
 
-@task
-def local_task(c):
-    c.run("uname -a")
+def local_task():
+    group.run("uname -a")
 
 
-@task
-def remote_task(c):
-    # 两个命令
-    with c.run("/data/logs"):
-        c.run("ls -l")
+def remote_task():
+    group.run("cd / && ls -l")
+
+
+if __name__ == '__main__':
+    local_task()
+    remote_task()
